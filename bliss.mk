612c8486211d01256f2b6cbeb73f6ebe5c7c40cf
alexander3244 <alexander324233@gmail.com>
2017-01-28 20:28:18 +0200

    Initial commit for Bliss MM ROM

--- bliss.mk
+++ bliss.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration for bowser
 $(call inherit-product, device/amazon/jem/full_jem.mk)
@@ -30,6 +30,6 @@ WITH_CM_CHARGER := false
 PRODUCT_PACKAGES += \
     Snap
 
-PRODUCT_NAME := cm_jem
+PRODUCT_NAME := bliss_jem
 PRODUCT_RELEASE_NAME := KindleFireHD
 
